2c5eb753054db1ee24ab4ab1709b19016d27d34b
Aluno <aluno@fiemguba.srv>
2026-06-02 19:52:03 -0300

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,16 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "admin",
   database: process.env.DB_DATABASE || "usuario",
-  pprt: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl:{
+    rejectUnauthorized: false,
+  }
 });
 
 pool.getConnection((err, connection) => {
