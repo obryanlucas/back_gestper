247d9a6583f93fb0eb401280b318c0ba21bd1bd2
Laboratório 203 <lab203@fiemguba.srv>
2026-06-23 21:20:56 -0300

    rota-enviar-email

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -1,22 +1,40 @@
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+});
 
 async function sendEmailToStudents(req, res) {
   try {
     const students = req.body.students;
 
     if (!students || students.length === 0) {
-      return res.status(400).json({ message: "Nenhum aluno informado" });
+      return res.status(400).json({
+        message: "Nenhum aluno informado",
+      });
     }
 
-    // Configuração do Gmail
+    const accessToken = await oauth2Client.getAccessToken();
+
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
+        type: "OAuth2",
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS, // App Password
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+        accessToken: accessToken.token,
       },
     });
-    console.log(transporter)
+
+    console.log(transporter);
 
     for (const student of students) {
       if (!student.email) continue;
@@ -29,11 +47,16 @@ async function sendEmailToStudents(req, res) {
       });
     }
 
-    return res.status(200).json({ message: "Emails enviados com sucesso!" });
+    return res.status(200).json({
+      message: "Emails enviados com sucesso!",
+    });
 
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "Erro ao enviar emails" });
+
+    return res.status(500).json({
+      message: "Erro ao enviar emails",
+    });
   }
 }
 
